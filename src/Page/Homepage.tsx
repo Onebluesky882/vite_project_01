2e70b1b5cf2cdb24957710b51a4f55466ef88166
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-06-20 20:20:16 +0700

    10th commit

--- src/Page/Homepage.tsx
+++ src/Page/Homepage.tsx
@@ -1,6 +1,3 @@
-import ProductCard from "../Components/ProductCard";
-import { Pork } from "../Data/pork";
-
 const HomePage = () => {
   return <h1 style={{ textAlign: "center", padding: "100px" }}>Homepage</h1>;
 };
