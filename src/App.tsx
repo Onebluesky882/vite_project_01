7433a6718af182b6ea101d870b256d800379106b
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-06-20 20:22:48 +0700

    10th commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Components/Layout";
-import Detail from "./Page/Seafood";
 import HomePage from "./Page/Homepage";
-import Desert from "./Page/PorkPage";
-import Drinks from "./Page/Drink";
 import PorkPage from "./Page/PorkPage";
 
 function App() {
@@ -14,9 +11,6 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="/pork" element={<PorkPage />} />
-          <Route path="/desert" element={<Desert />} />
-          <Route path="/drink" element={<Drinks />} />
-          <Route path="/detail" element={<Detail />} />
         </Route>
       </Routes>
     </BrowserRouter>
