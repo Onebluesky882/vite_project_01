5221a6f016548dbbaf6ba3cac8e693b7d0053d20
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-06-25 16:17:18 +0700

    14th commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="https://arroydee.netlify.app/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="/pork" element={<PorkMenu />} />
           <Route path="/beef" element={<BeefMenu />} />
